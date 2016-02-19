e69f402890312ba73b6025306f1f6541850fca6b
ZacWu <zw2338@columbia.edu>
2016-02-18 22:17:03 -0500

    upload a Tool Script
    uploaded SplitLatLng, using for split string with Lat Lng

---
+++ lib/Tool Script/splitLatLng.R
@@ -0,0 +1,26 @@
+crime <- read.csv("NYPD_7_Major_Felony_Incidents.csv")
+library("ggmap")
+
+a<-as.character(crime$Location.1)
+b<-as.numeric(unlist(strsplit(gsub("[^0-9. ]","", unlist(a)),"[^0-9.]")))
+
+lat = vector(length = length(b)/2)
+lng = vector(length = length(b)/2)
+count1 = 0
+count2 = 0
+for(i in 1:length(b)){
+        print(i)
+        if ((i%%2)!=0){
+                count1 = count1 + 1
+                lat[count1] = b[i]
+        }
+        else{
+                count2 = count2 + 1
+                lng[count2] = b[i]
+        }
+        
+}
+crime$lat <- lat
+crime$lng <- lng
+
+

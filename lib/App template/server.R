13a497145d47273b40ced92ff90c37b0e3918458
ZacWu <zw2338@columbia.edu>
2016-02-19 17:02:07 -0500

    modified server.r
    1.combine with UI
    2.address input interactive
    3.layout control

--- lib/App template/server.R
+++ lib/App template/server.R
@@ -37,7 +37,7 @@ shinyServer(function(input,output){
         }
         
         
-        
+
         
         
         
@@ -48,75 +48,84 @@ shinyServer(function(input,output){
         
         
         ##################################################################        
-        
-        
-        
         address <- "155 Claremont Ave, New York"
-        geocode <- geocode(address)
-        start_lat<-as.numeric(geocode$lat)
-        start_lng<-as.numeric(geocode$lon)
-        startpoint <- c(start_lat,start_lng,"startpoint","","")
+        sightsRanked <- reactive({data.frame(40.81364, -73.96120, "StartPoint","")})
         
-        
-        mustGo<-read.csv("Must-Go-Sights.csv")
-        randomSights <- read.csv("Must-Go-Sights.csv")#take out the mustGoSights
-        numberOfPlaces <- 5
-        
-        neighbor <- "b"
-        mustGo$neighbor <- c("a","b","a","a") #need to delete
-        randomSights<-mustGo #need to delete
-        #randomSights$NAME<-c("abc")#need to delete
-        
-        #select in neighborhood
-        mustGoSelected <- mustGo[mustGo$neighbor == neighbor,]
-        
-        #sampling
-        if(nrow(mustGoSelected) >= numberOfPlaces){
-                mustGoSelected<-mustGoSelected[sample(1:nrow(mustGoSelected),
-                                                      numberOfPlaces,replace=FALSE),]
-        }else{
-                n<-numberOfPlaces-nrow(mustGoSelected)
-                extra <- randomSights[sample(1:nrow(randomSights),n,replace=FALSE),]
-                mustGoSelected <- rbind(mustGoSelected,extra)      
-        }
-        #add start-end point
-        
-        mustGoSelected <- rbind(startpoint,mustGoSelected)
-        
-        #distance
-        distance <- dist(mustGoSelected[,1:2]) #approximation
-        
-        #tsp
-        tsp <- TSP(distance)
-        tour <- solve_TSP(tsp, method = "farthest_insertion")
-        path <- c(1,as.vector(cut_tour(tour, 1)))
-        
-        sightsRanked<-mustGoSelected[path,]
-        sightsRanked <- rbind(sightsRanked,sightsRanked[1,])
-        sightsRanked[,1]<-as.numeric(sightsRanked[,1])
-        sightsRanked[,2]<-as.numeric(sightsRanked[,2])
-        
+        sightsRanked <- eventReactive(input$recalc,{
+                if(isolate(input$location)!=""){
+                        address <-isolate(input$location)
+                }
+                geocode <- geocode(address)
+                start_lat<-as.numeric(geocode$lat)
+                start_lng<-as.numeric(geocode$lon)
+                startpoint <- c(start_lat,start_lng,"startpoint","","")
+                
+                
+                mustGo<-read.csv("Must-Go-Sights.csv")
+                randomSights <- read.csv("Must-Go-Sights.csv")#take out the mustGoSights
+                numberOfPlaces <- 5
+                
+                neighbor <- "b"
+                mustGo$neighbor <- c("a","b","a","a") #need to delete
+                randomSights<-mustGo #need to delete
+                #randomSights$NAME<-c("abc")#need to delete
+                
+                #select in neighborhood
+                mustGoSelected <- mustGo[mustGo$neighbor == neighbor,]
+                
+                #sampling
+                if(nrow(mustGoSelected) >= numberOfPlaces){
+                        mustGoSelected<-mustGoSelected[sample(1:nrow(mustGoSelected),
+                                                              numberOfPlaces,replace=FALSE),]
+                }else{
+                        n<-numberOfPlaces-nrow(mustGoSelected)
+                        extra <- randomSights[sample(1:nrow(randomSights),n,replace=FALSE),]
+                        mustGoSelected <- rbind(mustGoSelected,extra)      
+                }
+                #add start-end point
+                mustGoSelected <- rbind(startpoint,mustGoSelected)
+                
+                #distance
+                distance <- dist(mustGoSelected[,1:2]) #approximation
+                
+                #tsp
+                tsp <- TSP(distance)
+                tour <- solve_TSP(tsp, method = "farthest_insertion")
+                path <- c(1,as.vector(cut_tour(tour, 1)))
+                
+                sightsRanked<-mustGoSelected[path,]
+                sightsRanked <- rbind(sightsRanked,sightsRanked[1,])
+                sightsRanked[,1]<-as.numeric(sightsRanked[,1])
+                sightsRanked[,2]<-as.numeric(sightsRanked[,2])
+                
+                sightsRanked
+                
+        }, ignoreNULL = FALSE)
+       
         #plot 
         
-        points <- eventReactive(input$recalc, {
-                sightsRanked[,1:2]
-        }, ignoreNULL = FALSE)
-        
+       
+
         
 #############################################################################################################
         
         
         #output
         output$backgroup <- renderLeaflet({
+                print("backgroup")
                 leaflet() %>%
+                hideGroup(c("Views","Routes"))%>%
                 addProviderTiles("Stamen.Toner")%>%
                  # addTiles("Stamen.Toner") %>%  # Add default OpenStreetMap map tiles
                 addPolygons(data=shapeData, fillColor = colors[colorsmatched],
                             fillOpacity=0.8, stroke = FALSE,popup=content)%>%
                 #addMarkers(lng=-73.985428, lat=40.748817, popup="The Starting Point")
-                addMarkers(data = points())%>%
-                addPolylines(data = sightsRanked, lng = ~lng, lat = ~lat, group = ~NAME)
-                
+                addMarkers(data = sightsRanked(),popup = ~NAME,group = "Views")%>%
+                addPolylines(data = sightsRanked(), lng = ~lng, lat = ~lat, group = "Routes")%>%
+                addLayersControl(
+                        overlayGroups = c("Views", "Routes"),
+                        options = layersControlOptions(collapsed = FALSE)
+                )
         })
 })
 

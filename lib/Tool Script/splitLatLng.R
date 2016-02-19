2a308c8d2fd01742e0f842e562253d23e2180e5d
ZacWu <zw2338@columbia.edu>
2016-02-18 22:26:49 -0500

    small change in script

--- lib/Tool Script/splitLatLng.R
+++ lib/Tool Script/splitLatLng.R
@@ -8,7 +8,7 @@ lng = vector(length = length(b)/2)
 count1 = 0
 count2 = 0
 for(i in 1:length(b)){
-        print(i)
+    
         if ((i%%2)!=0){
                 count1 = count1 + 1
                 lat[count1] = b[i]

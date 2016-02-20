5d2a14589d09baf0b6bf16e913eb5ded2f4c1997
arsenal4ever <ys2901@columbia.edu>
2016-02-20 11:23:43 -0500

    update css
    transparent

--- lib/App template/ui.R
+++ lib/App template/ui.R
@@ -25,35 +25,32 @@ bootstrapPage (
         tags$head( includeCSS("styles.css")),
         leafletOutput("backgroup",width = "100%",height= "100%"),
         
+        absolutePanel(id = "output.graphs",fixed = T,draggable = TRUE, top = "5%", left = 20, right = "auto", bottom = "auto",
+                      width = 250, height = "auto",
+                      h2("Information"),
+                      plotOutput("hist", height = 200)),
+        
         #input
-        absolutePanel(bottom = "5%", right = "10%",left = "10%",draggable=TRUE,width="100%" ,height="10%",
-          splitLayout(
-                  sliderInput(inputId = "crime",label="I care safety", value = 5, min=0, max = 10),
-                  sliderInput(inputId = "wifi",label="I have to be online!", value = 5, min=0, max = 10),
-                  sliderInput(inputId = "restuarant",label="I eat goods", value = 5, min=0, max = 10)
-                  
-          )
-        ),
-     div(class = "set1",
+
         absolutePanel(id = "controls", fixed = TRUE,
                       draggable = TRUE, top = "5%", left = "auto", right = 20, bottom = "auto",
                       width = 250, height = "auto",
                       
-                      h2("Trip Planner"),
-                      
+                      h1("Trip Planner"),
+                      textInput(inputId = "location",label = "Where you are"),
+                      sliderInput(inputId = "crime",label="Safety", value = 5, min=0, max = 10),
+                      sliderInput(inputId = "wifi",label="Wifi", value = 5, min=0, max = 10),
+                      sliderInput(inputId = "restuarant",label="Resturant", value = 5, min=0, max = 10),
                       selectInput("category1","Category1",type),
                       selectInput("category2","Category2",type),
                       #selectInput("number","number",vars,selected = "art"),
                       sliderInput(inputId = "number",label="number", value = 5, min=1, max = 10),
-                      textInput(inputId = "location",label = "Where you are"),
+                    
                       #conditionalPanel(),
-                      actionButton("recalc", "My Trip Plan"),
-                      
-                      plotOutput("hist", height = 200)
+                      actionButton("recalc", "My Trip Plan")
                       #plotOutput("scatterCollegeIncome", height = 250)
                       
         )
-      )
     )
 )
 
